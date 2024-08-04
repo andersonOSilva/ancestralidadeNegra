369958b0df7ebf48e0f6734fa87887e7ee52a25e
132anderson <132anderson@gmail.com>
2024-08-04 03:02:49 -0300

    mark header and intro home

--- projeto_an/src/app/page.tsx
+++ projeto_an/src/app/page.tsx
@@ -4,10 +4,31 @@ import Header from "./components/header"
 export default function Home() {
   return (
     <>
-      <h1>
+      <div className="header">
+          <div className="menu"></div>
+          <div className="middle"></div>
+          <div className="register"></div>
+      </div>
+      <div className="intro">
+        <div className="strap">
+          <div className="arrow"></div>
+          <div className="question"></div>
+        </div>
+        <div className="text"></div>
 
-      chamar componente
-      </h1>
+      </div>
+      <div>
+          slide
+      </div>
+      <div>
+          galeria
+      </div>
+      <div>
+          range
+      </div>
+      <div>
+          formulario de escolas
+      </div>
     </>
      
     
